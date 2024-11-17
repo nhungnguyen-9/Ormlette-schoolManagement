1f7e3dadc9cee38e069dcb286b1a6b8e3ff2f1a0
nhungnguyen-9 <nguyennhung9846@gmail.com>
2024-11-17 22:43:57 +0700

    add navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,28 @@
+import { Bell, CircleUserRound, MessageCircleMore, Search } from "lucide-react"
 
 export const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <div className="flex items-center justify-between p-4">
+            {/* search */}
+            <div className="hidden md:flex items-center gap-2 text-xs rounded-full ring-[1.5px] ring-gray-300 px-2">
+                <Search className="size-4" />
+                <input type="text" placeholder="Search..." className="w-[200px] p-2 bg-transparent outline-none" />
+            </div>
+            {/* icon and user */}
+            <div className="flex items-center gap-6 justify-end w-full">
+                <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer">
+                    <MessageCircleMore className="size-6" />
+                </div>
+                <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer relative">
+                    <Bell className="size-6" />
+                    <div className="absolute -top-3 -right-3 w-5 h-5 flex items-center justify-center bg-yellow-500 text-white rounded-full">1</div>
+                </div>
+                <div className="flex flex-col">
+                    <span className="text-sm leading-3 font-medium">John Doe</span>
+                    <span className="text-[10px] text-gray-500 text-right">Admin</span>
+                </div>
+                <CircleUserRound className="size-6" />
+            </div>
+        </div>
     )
 }
