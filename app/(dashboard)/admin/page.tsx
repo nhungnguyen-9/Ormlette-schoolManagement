6222bd113ba250c059d7e1a4c08d6dde205d324b
nhungnguyen-9 <nguyennhung9846@gmail.com>
2024-11-19 19:40:47 +0700

    add radial chart

--- app/(dashboard)/admin/page.tsx
+++ app/(dashboard)/admin/page.tsx
@@ -1,18 +1,30 @@
+import { CountChart } from "@/components/count-chart";
 import { UserCard } from "@/components/user-card";
 
 const AdminPage = () => {
     return (
         <div className="p-4 flex gap-4 flex-col md:flex-flow">
             {/* left */}
-            <div className="w-full lg:w-2/3">
-                l
-            </div>
-            {/* usercard */}
-            <div className="flex gap-4 justify-between flex-wrap">
-                <UserCard type="student" />
-                <UserCard type="teacher" />
-                <UserCard type="parent" />
-                <UserCard type="staff" />
+            <div className="w-full lg:w-2/3 flex flex-col gap-8">
+
+                {/* usercard */}
+                <div className="flex gap-4 justify-between flex-wrap">
+                    <UserCard type="student" />
+                    <UserCard type="teacher" />
+                    <UserCard type="parent" />
+                    <UserCard type="staff" />
+                </div>
+                {/* middle chart */}
+                <div className="flex gap-4 flex-col lg:flex-row">
+                    {/* count chart */}
+                    <div className="w-full lg:w-1/3 h-[450px]">
+                        <CountChart />
+                    </div>
+                    {/* attendance chart */}
+                    <div className="w-full lg:w-2/3 h-[450px]"></div>
+                </div>
+                {/* bottom chart */}
+                <div className=""></div>
             </div>
             {/* right */}
             <div className="w-full lg:w-1/3">
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,11 @@ export default {
 	],
 	theme: {
 		extend: {
+			backgroundImage: {
+				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+				"gradient-conic":
+					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+			},
 			colors: {
 				background: 'hsl(var(--background))',
 				foreground: 'hsl(var(--foreground))',
