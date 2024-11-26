2cda2faf1fa2f797b33d9768cb2e055b5d2f4a82
nhungnguyen-9 <nguyennhung9846@gmail.com>
2024-11-26 19:46:39 +0700

    add reuseable table

--- app/(dashboard)/list/teachers/page.tsx
+++ app/(dashboard)/list/teachers/page.tsx
@@ -1,6 +1,11 @@
+import { FormModal } from "@/components/form-modal";
 import { Pagination } from "@/components/pagination";
+import { Table } from "@/components/table";
 import { TableSearch } from "@/components/table-search";
-import { ArrowDownUp, Plus, SlidersHorizontal } from "lucide-react";
+import { role, teachersData } from "@/lib/data";
+import { ArrowDownUp, Eye, Plus, SlidersHorizontal } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 
 type Teacher = {
     id: number;
@@ -18,6 +23,7 @@ const columns = [
     {
         header: "Info",
         accessor: "info",
+        className: 'pl-5'
     },
     {
         header: "Teacher ID",
@@ -51,6 +57,43 @@ const columns = [
 ];
 
 const TeacherListPage = () => {
+    const renderRow = (item: Teacher) => (
+        <tr
+            key={item.id}
+            className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaSkyLight"
+        >
+            <td className="flex items-center gap-4 p-4">
+                <Image
+                    src={item.photo}
+                    alt=""
+                    width={40}
+                    height={40}
+                    className="md:hidden xl:block w-10 h-10 rounded-full object-cover"
+                />
+                <div className="flex flex-col">
+                    <h3 className="font-semibold">{item.name}</h3>
+                    <p className="text-xs text-gray-500">{item?.email}</p>
+                </div>
+            </td>
+            <td className="hidden md:table-cell">{item.teacherId}</td>
+            <td className="hidden md:table-cell">{item.subjects.join(",")}</td>
+            <td className="hidden md:table-cell">{item.classes.join(",")}</td>
+            <td className="hidden md:table-cell">{item.phone}</td>
+            <td className="hidden md:table-cell">{item.address}</td>
+            <td>
+                <div className="flex items-center gap-2">
+                    <Link href={`/list/teachers/${item.id}`}>
+                        <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaGreen">
+                            <Eye className="size-5" />
+                        </button>
+                    </Link>
+                    {role === "admin" && (
+                        <FormModal table="teacher" type="delete" id={item.id} />
+                    )}
+                </div>
+            </td>
+        </tr>
+    );
     return (
         <div className="bg-white rounded-md flex-1 m-4 mt-0">
             {/* top */}
@@ -72,9 +115,7 @@ const TeacherListPage = () => {
                 </div>
             </div>
             {/* list */}
-            <div className="">
-
-            </div>
+            <Table columns={columns} renderRow={renderRow} data={teachersData} />
             {/* pagination */}
             <Pagination />
         </div>
