4efd2fb2a250cedac60eac9ab8b3e7169d32672e
nhungnguyen-9 <nguyennhung9846@gmail.com>
2024-12-02 17:16:38 +0700

    fetch data with query params on the url

--- app/(dashboard)/list/students/[id]/page.tsx
+++ app/(dashboard)/list/students/[id]/page.tsx
@@ -122,7 +122,7 @@ const SingleStudentPage = () => {
                         <Link className="p-3 rounded-md bg-lamaSkyLight" href="/">
                             Student&apos;s Lessons
                         </Link>
-                        <Link className="p-3 rounded-md bg-lamaGreenLight" href="/">
+                        <Link className="p-3 rounded-md bg-lamaGreenLight" href={`/list/teachers?classId={2}`}>
                             Student&apos;s Teachers
                         </Link>
                         <Link className="p-3 rounded-md bg-pink-50" href="/">
--- app/(dashboard)/list/teachers/page.tsx
+++ app/(dashboard)/list/teachers/page.tsx
@@ -5,7 +5,7 @@ import { TableSearch } from "@/components/table-search";
 import { role } from "@/lib/data";
 import prisma from "@/lib/db";
 import { ITEM_PER_PAGE } from "@/lib/settings";
-import { Class, Subject, Teacher } from "@prisma/client";
+import { Class, Prisma, Subject, Teacher } from "@prisma/client";
 import { ArrowDownUp, Eye, Plus, SlidersHorizontal } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
@@ -96,8 +96,27 @@ const TeacherListPage = async ({
 
     const p = page ? parseInt(page) : 1
 
+    // url params condition
+    const query: Prisma.TeacherWhereInput = {}
+
+    if (queryParams) {
+        for (const [key, value] of Object.entries(queryParams)) {
+            if (value !== undefined) {
+                switch (key) {
+                    case 'classId':
+                        query.lessons = {
+                            some: {
+                                classId: parseInt(value)
+                            }
+                        }
+                }
+            }
+        }
+    }
+
     const [teachers, count] = await prisma.$transaction([
         prisma.teacher.findMany({
+            where: query,
             include: {
                 subjects: true,
                 classes: true
@@ -105,7 +124,7 @@ const TeacherListPage = async ({
             take: ITEM_PER_PAGE,
             skip: ITEM_PER_PAGE * (p - 1)
         }),
-        prisma.teacher.count()
+        prisma.teacher.count({ where: query })
     ])
 
     return (
