66a68d66b602ca813388fda858afacbff35b9280
nhungnguyen-9 <nguyennhung9846@gmail.com>
2024-12-05 21:04:55 +0700

    clerk role based auth

---
+++ app/[[...sign-in]]/page.tsx
@@ -0,0 +1,66 @@
+'use client'
+
+import * as Clerk from '@clerk/elements/common'
+import * as SignIn from '@clerk/elements/sign-in'
+import { useUser } from '@clerk/nextjs'
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
+
+export default function LoginPage() {
+  const { isLoaded, isSignedIn, user } = useUser()
+
+  const router = useRouter()
+
+  useEffect(() => {
+    const role = user?.publicMetadata.role
+    if (role) {
+      router.push(`/${role}`)
+    }
+  }, [user, router])
+
+  return (
+    <div className="h-screen flex items-center justify-center bg-lamaSkyLight">
+      <SignIn.Root>
+        <SignIn.Step
+          name="start"
+          className="bg-white p-12 rounded-md shadow-2xl flex flex-col gap-2"
+        >
+          <h1 className="text-xl font-bold flex items-center gap-2">
+            <Image src="/new.png" alt="" width={120} height={120} />
+          </h1>
+          <h2 className="text-gray-400">Sign in to your account</h2>
+          <Clerk.GlobalError className="text-sm text-red-400" />
+          <Clerk.Field name="identifier" className="flex flex-col gap-2 mt-1">
+            <Clerk.Label className="text-xs text-gray-500">
+              Email
+            </Clerk.Label>
+            <Clerk.Input
+              type="email"
+              required
+              className="p-2 rounded-xl ring-1 ring-gray-300"
+            />
+            <Clerk.FieldError className="text-xs text-red-400" />
+          </Clerk.Field>
+          <Clerk.Field name="password" className="flex flex-col gap-2 mt-1">
+            <Clerk.Label className="text-xs text-gray-500">
+              Password
+            </Clerk.Label>
+            <Clerk.Input
+              type="password"
+              required
+              className="p-2 rounded-xl ring-1 ring-gray-300"
+            />
+            <Clerk.FieldError className="text-xs text-red-400" />
+          </Clerk.Field>
+          <SignIn.Action
+            submit
+            className="bg-blue-500 text-white my-2 rounded-lg text-sm p-[10px]"
+          >
+            Sign In
+          </SignIn.Action>
+        </SignIn.Step>
+      </SignIn.Root>
+    </div>
+  )
+}
